1e4163658fc978a416e8d339abdaa3c285282c1d
Gustavo Rosa <gustavorib.rosa@gmail.com>
2024-05-07 14:38:04 -0300

    few stylization, not done yet

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,23 @@
-import '../styles/Task.css'
-
 const Task = (props) => {
     return (
-        <div>
-            <h1>{props.taskName}</h1>
-            {/* a function with a parameter needs to be called inside a anonymous function to work */}
-            <button onClick={() => props.deleteTask(props.id)}>
-                X
-            </button>
-            <button onClick={() => props.editTask(props.id)}>
-                Edit
-            </button>
+        <div className="card mb-3 mx-auto" style={{ width: "35rem" }}>
+            <div className="card-body">
+                <p className="card-subtitle mb-2 text-muted">
+                    {props.taskName}
+                </p>
+
+                {/* a function with a parameter needs to be called inside a anonymous function to work */}
+                <button
+                    className="btn btn-danger btn-sm mt-2"
+                    onClick={() => props.deleteTask(props.id)}>
+                    X
+                </button>
+                <button
+                    className="btn btn-primary"
+                    onClick={() => props.editTask(props.id)}>
+                    Edit
+                </button>
+            </div>
         </div>
     )
 
